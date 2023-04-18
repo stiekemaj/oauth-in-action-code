06a9f0b94f7fdf7caa0991bc66445482ca068641
Jeroen Stiekema <jeroen@stiekema.net>
2023-04-18 08:33:20 +0200

    Chapter 3, exercise 1

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -27,8 +27,8 @@ var authServer = {
  * Add the client information in here
  */
 var client = {
-	"client_id": "",
-	"client_secret": "",
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
 	"redirect_uris": ["http://localhost:9000/callback"]
 };
 
@@ -48,7 +48,17 @@ app.get('/authorize', function(req, res){
 	/*
 	 * Send the user to the authorization server
 	 */
-	
+
+	state = randomstring.generate();
+
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0],
+		state: state
+	});
+
+	res.redirect(authorizeUrl);
 });
 
 app.get('/callback', function(req, res){
@@ -56,7 +66,36 @@ app.get('/callback', function(req, res){
 	/*
 	 * Parse the response from the authorization server and get a token
 	 */
-	
+
+	if (state !== req.query.state) {
+		res.render('error', {error: 'State value did not match'})
+		return;
+	}
+
+	var code = req.query.code;
+
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint, {
+		body: form_data,
+		headers: headers
+	});
+
+	var body = JSON.parse(tokRes.getBody());
+
+	access_token = body.access_token;
+
+	res.render('index', {access_token: access_token, scope: scope});
+
 });
 
 app.get('/fetch_resource', function(req, res) {
@@ -64,6 +103,23 @@ app.get('/fetch_resource', function(req, res) {
 	/*
 	 * Use the access token to call the resource server
 	 */
+
+	if (!access_token) {
+		res.render('error', {error: 'Missing access token'});
+		return;
+	}
+
+	var headers = {
+		'Authorization': 'Bearer ' + access_token
+	};
+
+	var resourceRes = request('POST', protectedResource, {
+		headers: headers
+	});
+
+	var body = JSON.parse(resourceRes.getBody());
+
+	res.render('data', {resource: body});
 	
 });
 
